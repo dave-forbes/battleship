ab23775341e0a431958c03b2506c922c388a5d0b
David Forbes <davidforbesi@hotmail.com>
2023-11-13 10:54:22 +0000

    added player gameboard test and corresponding code

--- src/player.js
+++ src/player.js
@@ -1,5 +1,8 @@
+import Gameboard from "./gameboard";
+
 export default class Player {
   constructor(computer) {
     this.computer = computer === "computer" ? true : false;
+    this.gameboard = new Gameboard();
   }
 }
--- src/player.test.js
+++ src/player.test.js
@@ -8,3 +8,13 @@ test("Player creates a human or computer player object.", () => {
   expect(humanPlayer.computer).toBe(false);
   expect(computerPlayer.computer).toBe(true);
 });
+
+test("Player creates an object that contains its own gameboard.", () => {
+  const humanPlayer = new Player();
+  const computerPlayer = new Player("computer");
+  expect(typeof humanPlayer.gameboard).toBe("object");
+  expect(typeof computerPlayer.gameboard).toBe("object");
+  expect(humanPlayer.gameboard !== computerPlayer.gameboard).toBe(true);
+});
+
+test("Players can take turns playing the game by attacking the enemy Gameboard.", () => {});
