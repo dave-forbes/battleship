fe1e53fa5818548228b3aaae79456bf9e7eb18eb
David Forbes <davidforbesi@hotmail.com>
2023-11-13 15:46:24 +0000

    added random generate ships method to player.js

--- src/player.js
+++ src/player.js
@@ -19,8 +19,8 @@ export default class Player {
       }
       // Human Player attack
     } else {
-      const x = Math.floor(Math.random() * 10) + 1;
-      const y = Math.floor(Math.random() * 10) + 1;
+      const x = Math.floor(Math.random() * 10);
+      const y = Math.floor(Math.random() * 10);
       const coOrds = [x, y];
       if (opposingPlayer.gameboard.recieveAttack(coOrds)) {
         opposingPlayer.gameboard.recieveAttack(coOrds);
@@ -32,5 +32,82 @@ export default class Player {
       }
       //Computer Player attack
     }
+  }
+
+  randomGenerateShips() {
+    const isVertical = () => (Math.random() > 0.5 ? true : false);
+
+    const fourSqaureShip = () => {
+      if (isVertical()) {
+        const x = Math.floor(Math.random() * 10);
+        const y = Math.floor(Math.random() * 8);
+        this.gameboard.placeShip([x, y], [x, y + 1], [x, y + 2], [x, y + 3]);
+      } else {
+        const y = Math.floor(Math.random() * 10);
+        const x = Math.floor(Math.random() * 8);
+        this.gameboard.placeShip([x, y], [x + 1, y], [x + 2, y], [x + 3, y]);
+      }
+    };
+    const threeSqaureShip = () => {
+      if (isVertical()) {
+        let sucess = false;
+        while (sucess === false) {
+          const x = Math.floor(Math.random() * 10);
+          const y = Math.floor(Math.random() * 8);
+          sucess = this.gameboard.placeShip([x, y], [x, y + 1], [x, y + 2]);
+        }
+      } else {
+        let sucess = false;
+        while (sucess === false) {
+          const y = Math.floor(Math.random() * 10);
+          const x = Math.floor(Math.random() * 8);
+          sucess = this.gameboard.placeShip([x, y], [x + 1, y], [x + 2, y]);
+        }
+      }
+    };
+    const twoSqaureShip = () => {
+      if (isVertical()) {
+        let sucess = false;
+        while (sucess === false) {
+          const x = Math.floor(Math.random() * 10);
+          const y = Math.floor(Math.random() * 8);
+          sucess = this.gameboard.placeShip([x, y], [x, y + 1]);
+        }
+      } else {
+        let sucess = false;
+        while (sucess === false) {
+          const y = Math.floor(Math.random() * 10);
+          const x = Math.floor(Math.random() * 8);
+          sucess = this.gameboard.placeShip([x, y], [x + 1, y]);
+        }
+      }
+    };
+    const oneSqaureShip = () => {
+      if (isVertical()) {
+        let sucess = false;
+        while (sucess === false) {
+          const x = Math.floor(Math.random() * 10);
+          const y = Math.floor(Math.random() * 8);
+          sucess = this.gameboard.placeShip([x, y]);
+        }
+      } else {
+        let sucess = false;
+        while (sucess === false) {
+          const y = Math.floor(Math.random() * 10);
+          const x = Math.floor(Math.random() * 8);
+          sucess = this.gameboard.placeShip([x, y]);
+        }
+      }
+    };
+    fourSqaureShip();
+    threeSqaureShip();
+    threeSqaureShip();
+    twoSqaureShip();
+    twoSqaureShip();
+    twoSqaureShip();
+    oneSqaureShip();
+    oneSqaureShip();
+    oneSqaureShip();
+    oneSqaureShip();
   }
 }
