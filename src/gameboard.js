6cd28a5bebcbcdcba16b13f8e7d61a9e9b3bcf75
David Forbes <davidforbesi@hotmail.com>
2023-11-13 12:51:22 +0000

    changed generateboard to generate only a 10x10 array instead of 11x11, updated boardIndex

--- src/gameboard.js
+++ src/gameboard.js
@@ -10,8 +10,8 @@ export default class Gameboard {
 
   generateBoard() {
     let board = [];
-    for (let i = 0; i <= 10; i++) {
-      for (let j = 0; j <= 10; j++) {
+    for (let i = 1; i <= 10; i++) {
+      for (let j = 1; j <= 10; j++) {
         board.push([i, j]);
       }
     }
