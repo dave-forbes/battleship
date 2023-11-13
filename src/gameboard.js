f495a5e45b088b308760c25a5857e4a920f22d9f
David Forbes <davidforbesi@hotmail.com>
2023-11-13 09:52:38 +0000

    place ship tests and corresponding gameboard methods

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,17 +2,49 @@ import Ship from "./ship";
 
 export default class Gameboard {
   constructor() {
-    this.xMin = "A";
-    this.xMax = "J";
-    this.yMin = "0";
-    this.yMax = "10";
-    this.ships = [];
-    this.hits = [];
-    this.misses = [];
+    this.board = this.generateBoard();
+  }
+
+  generateBoard() {
+    let board = [];
+    for (let i = 0; i <= 10; i++) {
+      for (let j = 0; j <= 10; j++) {
+        board.push([i, j]);
+      }
+    }
+    return board;
+  }
+
+  boardIndex([x, y]) {
+    return x * 11 + y;
   }
 
   placeShip(...coOrds) {
     const ship = new Ship(coOrds.length);
-    this.ships.push([coOrds, ship]);
+    const coordinates = [...coOrds];
+
+    for (const coordinate of coordinates) {
+      for (const cell of this.board) {
+        if (
+          coordinate[0] === cell[0] &&
+          coordinate[1] === cell[1] &&
+          cell.length === 3
+        ) {
+          return false;
+          // Already occupied
+        }
+      }
+    }
+    for (const coordinate of coordinates) {
+      for (const cell of this.board) {
+        if (coordinate[0] === cell[0] && coordinate[1] === cell[1]) {
+          cell.push(ship);
+          // Not occupied so add ship
+        }
+      }
+    }
+    return true;
   }
+
+  recieveAttack([x, y]) {}
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -2,11 +2,35 @@ import Gameboard from "./gameboard";
 
 test("Gameboards should be able to place ships at specific coordinates by calling the ship factory function.", () => {
   const gameboard = new Gameboard();
-  gameboard.placeShip(["A", 1], ["B", 1]);
-  expect(gameboard.ships[0].length).toBe(2);
-  expect(typeof gameboard.ships[0]).toBe("object");
-  expect(gameboard.ships[0][0]).toStrictEqual([
-    ["A", 1],
-    ["B", 1],
-  ]);
+  gameboard.placeShip([0, 1], [1, 1]);
+  expect(typeof gameboard.board[gameboard.boardIndex([0, 1])][2]).toBe(
+    "object"
+  );
+  expect(typeof gameboard.board[gameboard.boardIndex([1, 1])][2]).toBe(
+    "object"
+  );
+  expect(
+    Object.hasOwn(
+      typeof gameboard.board[gameboard.boardIndex([1, 1])][2],
+      "length"
+    )
+  ).toBe(true);
+  console.log(gameboard.board[gameboard.boardIndex([1, 1])]);
+});
+
+test("Can't place a ship over co-ordinates that are already occupied", () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip([0, 1], [1, 1]);
+  expect(gameboard.placeShip([0, 1], [1, 1])).toBe(false);
+  expect(gameboard.placeShip([1, 1], [1, 2], [2, 3])).toBe(false);
+  expect(gameboard.placeShip([3, 1], [3, 2], [3, 3])).toBe(true);
+});
+
+test.only("Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.", () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip([0, 1], [1, 1]);
+  gameboard.recieveAttack([0, 1]);
+  expect(gameboard.board[gameboard.boardIndex([0, 1])][2].hits).toBe(1);
+  gameboard.recieveAttack([0, 2]);
+  expect(gameboard.missedShots.has(JSON.stringify([0, 2]))).toBe(true);
 });
