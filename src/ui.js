4227af032050b28669a67f9a6e7d779a7b1c72d3
David Forbes <davidforbesi@hotmail.com>
2023-11-15 11:13:32 +0000

    separated factories from ui

--- src/ui.js
+++ src/ui.js
@@ -1,23 +1,41 @@
-import { humanPlayer, computerPlayer } from "./gameLoop.js";
+import {
+  humanPlayerBoard,
+  computerPlayerBoard,
+  playerTurn,
+} from "./gameLoop.js";
 
-// console.log(humanPlayer.gameboard.board);
-// console.log(computerPlayer.gameboard.board);
+console.log({ humanPlayerBoard, computerPlayerBoard });
 
-const humanPlayerBoard = document.querySelector("#human-player");
-console.log(humanPlayerBoard);
-const computerPlayerBoard = document.querySelector("#computer-player");
-console.log(computerPlayerBoard);
+const humanPlayerBoardUI = document.querySelector("#human-player");
 
-for (const coordinate of humanPlayer.gameboard.board) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  if (coordinate[2]) cell.classList.add("ship");
-  humanPlayerBoard.appendChild(cell);
-}
+const computerPlayerBoardUI = document.querySelector("#computer-player");
 
-for (const coordinate of computerPlayer.gameboard.board) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  if (coordinate[2]) cell.classList.add("ship");
-  computerPlayerBoard.appendChild(cell);
-}
+const generateDivsFromGameboard = (board, node) => {
+  for (const coordinate of board) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    if (coordinate[2] && node !== computerPlayerBoardUI)
+      cell.classList.add("ship");
+    cell.dataset.X = coordinate[0];
+    cell.dataset.Y = coordinate[1];
+    node.appendChild(cell);
+  }
+};
+
+generateDivsFromGameboard(humanPlayerBoard, humanPlayerBoardUI);
+generateDivsFromGameboard(computerPlayerBoard, computerPlayerBoardUI);
+
+const clickAttack = (e) => {
+  const x = Number(e.target.dataset.X);
+  const y = Number(e.target.dataset.Y);
+  const array = [x, y];
+  const attack = playerTurn(array);
+  const node = e.target;
+  if (attack === "Hit") {
+    node.classList.add("hit");
+  } else {
+    node.classList.add("miss");
+  }
+};
+
+computerPlayerBoardUI.addEventListener("click", clickAttack);
