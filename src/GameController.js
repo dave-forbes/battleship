7eda49fff47139c3a28a3a7f72cffe75a85b82d4
David Forbes <davidforbesi@hotmail.com>
2023-11-17 11:55:33 +0000

    renamed gameloop.js, changed to factory function and exported

---
+++ src/GameController.js
@@ -0,0 +1,51 @@
+import Player from "./player.js";
+
+function GameController() {
+  const humanPlayer = new Player();
+  const computerPlayer = new Player("computer");
+
+  // humanPlayer.randomGenerateShips();
+  computerPlayer.randomGenerateShips();
+
+  const humanPlayerBoard = humanPlayer.gameboard.board;
+  const computerPlayerBoard = computerPlayer.gameboard.board;
+
+  const gameLoop = (coOrds) => {
+    humanPlayer.attack(computerPlayer, coOrds);
+    if (computerPlayer.gameboard.allShipsSunk())
+      return "Game over, player wins";
+    computerPlayer.attack(humanPlayer);
+    if (humanPlayer.gameboard.allShipsSunk())
+      return "Game over, computer wins!";
+  };
+
+  const dropShip = (coOrds, size, vertical) => {
+    const shipCoOrds = humanPlayer.gameboard.gerenateShipCoOrds(
+      coOrds,
+      size,
+      vertical
+    );
+    const newBoard = humanPlayer.gameboard.placeShip(...shipCoOrds);
+    const allShipsPlaced = humanPlayer.gameboard.allShipsPlaced();
+    return { newBoard, allShipsPlaced };
+  };
+
+  const allAttacksHuman = computerPlayer.gameboard.allAttacks;
+  const allMissesHuman = computerPlayer.gameboard.missedShots;
+  const allAttacksComputer = humanPlayer.gameboard.allAttacks;
+  const allMissesComputer = humanPlayer.gameboard.missedShots;
+
+  return {
+    humanPlayerBoard,
+    computerPlayerBoard,
+    allAttacksHuman,
+    allAttacksComputer,
+    allMissesHuman,
+    allMissesComputer,
+    gameLoop,
+    humanPlayer,
+    dropShip,
+  };
+}
+
+export default GameController;
