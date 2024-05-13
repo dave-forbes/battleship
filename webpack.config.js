9fdf41858d074fdda9580b9b573b8d17cf55f20a
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-13 14:05:55 +0100

    add dev server to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
-import { resolve } from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default {
-  entry: "./src/ui.js",
-  mode: "development",
+  entry: './src/ui.js',
+  mode: 'development',
   output: {
-    filename: "bundle.js",
-    path: resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: 'dist',
+    },
+    compress: true,
+    port: 9000,
   },
 };
